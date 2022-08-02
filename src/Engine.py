8983e2bcd86bc7ab18458f81186fc7344ae5f52a
tomvik <lugo.lbmi@gmail.com>
2022-08-02 14:26:09 -0700

    use ok

--- src/Engine.py
+++ src/Engine.py
@@ -10,16 +10,22 @@ import Common
 def Print(function_name: str, message: str):
     print('[{}]: {}'.format(function_name, message))
 
-async def OpenLabyrinthGenerator(headless: bool) -> Page:
+async def OpenLabyrinthGenerator(headless: bool) -> Tuple[Browser, Page, bool]:
     print("Opening labyrinth Generator page")
+    ok = False
 
-    browser = await launch(headless=headless, defaultViewport={'width': 1920, 'height': 1080})
-    page: Page = await browser.newPage()
+    try:
+        browser = await launch(headless=headless, defaultViewport={'width': 1920, 'height': 1080})
+        page: Page = await browser.newPage()
 
-    await page.goto(Common.PAGE_URL)
-    await page.screenshot({'path': 'data/main_page.png'})
+        await page.goto(Common.PAGE_URL)
+        await page.screenshot({'path': 'data/main_page.png'})
 
-    return browser, page
+        ok = True
+    except Exception as e:
+        Print('OpenLabyrinthGenerator', e)
+
+    return browser, page, ok
 
 async def CloseBrowser(browser: Browser) -> None:
     if browser is not None:
@@ -34,18 +40,19 @@ async def GetPageElement(page: Page, query: str) -> ElementHandle:
 
     return element
 
-async def GetInputElements(page: Page) -> Tuple[ElementHandle, ElementHandle]:
+async def GetInputElements(page: Page) -> Tuple[ElementHandle, ElementHandle, bool]:
+    ok = False
     try:
         rows_input_element = await GetPageElement(page, '[name="zeilenstr"]')
         columns_input_element = await GetPageElement(page, '[name="spaltenstr"]')
+
+        ok = True
     except TypeError as e:
         Print('GetInputElements', 'An input element was not found. Verify the page and query are correct. {}'.format(e))
-        raise e
     except Exception as e:
         Print('GetInputElements', 'Unkown error occured. {}'.format(e))
-        raise e
 
-    return rows_input_element, columns_input_element
+    return rows_input_element, columns_input_element, ok
 
 async def WriteInputValue(element: ElementHandle, txt: str) -> None:
     try:
@@ -59,37 +66,44 @@ async def WriteInputValue(element: ElementHandle, txt: str) -> None:
         Print('WriteInputValue', 'Unknown error: {}'.format(e))
         raise e
 
-async def WriteInputValues(rows_input: ElementHandle, columns_input: ElementHandle, num_rows: int, num_columns: int) -> None:
+async def WriteInputValues(rows_input: ElementHandle, columns_input: ElementHandle, num_rows: int, num_columns: int) -> bool:
+    ok = False
     try:
         await WriteInputValue(rows_input, str(num_rows))
         await WriteInputValue(columns_input, str(num_columns))
+
+        ok = True
     except Exception as e:
         Print('WriteInputValues', e)
+
+    return ok
 
 
 async def PlayGame():
     print("Playing game")
 
+    ok: bool = False
     browser: Browser = None
     page: Page = None
-    try:
-        browser, page = await OpenLabyrinthGenerator(False)
-    except:
-        Print('PlayGame', 'Failed opening the webpage')
+    browser, page, ok = await OpenLabyrinthGenerator(False)
+
+    if not ok:
         return
 
     rows_input: ElementHandle = None
     columns_input: ElementHandle = None
-    try:
-        rows_input, columns_input = await GetInputElements(page)
-    except TypeError as e:
-        Print('PlayGame', 'GetInputElements failed. {}'.format(e))
-        await CloseBrowser(browser)
-    except Exception as e:
-        Print('PlayGame', 'Unkown error occured. {}'.format(e))
-        await CloseBrowser(browser)
+    rows_input, columns_input, ok = await GetInputElements(page)
 
-    await WriteInputValues(rows_input, columns_input, 7, 8)
+    if not ok:
+        await CloseBrowser(browser)
+        return
+
+    ok = await WriteInputValues(rows_input, columns_input, 7, 8)
+
+    if not ok:
+        await CloseBrowser(browser)
+        return
+
     await page.screenshot({'path': 'data/main_page_after.png'})
 
     await CloseBrowser(browser)
