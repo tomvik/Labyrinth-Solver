f79d53e75c36d619cc8ba8f8cfad37fee9416ea2
tomvik <lugo.lbmi@gmail.com>
2022-08-02 13:59:44 -0700

    Get input elements

--- src/Engine.py
+++ src/Engine.py
@@ -1,12 +1,16 @@
 import asyncio
+from typing import Tuple
 from pyppeteer import launch
 from pyppeteer.browser import Browser
 from pyppeteer.page import Page
+from pyppeteer.element_handle import ElementHandle
 
 import Common
 
+def Print(function_name: str, message: str):
+    print('[{}]: {}'.format(function_name, message))
 
-async def OpenLabyrinthGenerator(browser: Browser, headless: bool) -> Page:
+async def OpenLabyrinthGenerator(headless: bool) -> Page:
     print("Opening labyrinth Generator page")
 
     browser = await launch(headless=headless, defaultViewport={'width': 1920, 'height': 1080})
@@ -15,21 +19,59 @@ async def OpenLabyrinthGenerator(browser: Browser, headless: bool) -> Page:
     await page.goto(Common.PAGE_URL)
     await page.screenshot({'path': 'data/main_page.png'})
 
-    return page
+    return browser, page
 
-def CloseBrowser(browser) -> None:
-    asyncio.get_event_loop().run_until_complete(browser.close())   
+async def CloseBrowser(browser: Browser) -> None:
+    if browser is not None:
+        await browser.close()
+        browser = None
 
-def PlayGame():
+async def GetPageElement(page: Page, query: str) -> ElementHandle:
+    element = await page.J(query)
+
+    if element is None:
+        raise TypeError("Element not found. Query: {}".format(query))
+
+    return element
+
+async def GetInputElements(page: Page) -> Tuple[ElementHandle, ElementHandle]:
+    try:
+        rows_input_element = await GetPageElement(page, '[name="zeilenstr"]')
+        columns_input_element = await GetPageElement(page, '[name="spaltenstr"]')
+    except TypeError as e:
+        Print('GetInputElements', 'An input element was not found. Verify the page and query are correct. {}'.format(e))
+        raise e
+    except Exception as e:
+        Print('GetInputElements', 'Unkown error occured. {}'.format(e))
+        raise e
+
+    return rows_input_element, columns_input_element
+
+async def PlayGame():
     print("Playing game")
 
-    browser = None
-    page = None
-    page = asyncio.get_event_loop().run_until_complete(OpenLabyrinthGenerator(browser, False))
-    print(page)
+    browser: Browser = None
+    page: Page = None
+    try:
+        browser, page = await OpenLabyrinthGenerator(False)
+    except:
+        Print('PlayGame', 'Failed opening the webpage')
+        return
 
-    CloseBrowser(browser)
+    rows_input: ElementHandle = None
+    columns_input: ElementHandle = None
+    try:
+        rows_input, columns_input = await GetInputElements(page)
+    except TypeError as e:
+        Print('PlayGame', 'GetInputElements failed. {}'.format(e))
+        await CloseBrowser(browser)
+    except Exception as e:
+        Print('PlayGame', 'Unkown error occured. {}'.format(e))
+        await CloseBrowser(browser)
+
+    await CloseBrowser(browser)
+    
 
 if __name__ == "__main__":
     Common.initialize_keyboard_listener()
-    PlayGame()
+    asyncio.run(PlayGame())
