6a90bd5e7d9eeca304e483ca2a9d468d6117c19f
tomvik <lugo.lbmi@gmail.com>
2022-08-03 15:30:59 -0700

    Read image and get start point

--- src/Engine.py
+++ src/Engine.py
@@ -1,11 +1,77 @@
+import cv2
 import Common
 from BrowserWrapper import OpenBrowserAndStoreCustomLabyrinth
 
-def PlayGame():
-    OpenBrowserAndStoreCustomLabyrinth(Common.NUM_ROWS, Common.NUM_COLUMNS, Common.LABYRINTH_PATH)
+from collections import namedtuple
+
+Point = namedtuple('Point', ['x', 'y'])
+
+def GetProcessedLabyrinthImage() -> cv2.Mat | None:
+    print('Reading stored labyrinth')
+    labyrinth: cv2.Mat | None = cv2.imread(Common.LABYRINTH_PATH, cv2.IMREAD_GRAYSCALE)
+
+    if labyrinth is None:
+        print('Labyrinth image not found')
+        return None
+
+    _, threshold = cv2.threshold(labyrinth, 150, 255, cv2.THRESH_BINARY)
+    binary_labyrinth = cv2.bitwise_and(labyrinth, labyrinth, mask=threshold)
+    labyrinth = cv2.cvtColor(binary_labyrinth, cv2.COLOR_GRAY2RGB)
+    
+    cv2.imshow('Labyrinth', labyrinth)
+    cv2.waitKey()
+
+    return labyrinth
+
+def GetStartPoint(labyrinth: cv2.Mat) -> Point:
+    # Find leftmost vertical wall
+    row = 30
+    col = 0
+    while tuple(labyrinth[row, col]) != Common.COLOR_BLACK:
+        labyrinth[row, col] = [0, 255, 0]
+        if col % 5 == 0:
+            cv2.imshow('Looking for line', labyrinth)
+            cv2.waitKey()
+
+        col += 1
+
+    print(row, col, 'Found black tile')
+
+    while tuple(labyrinth[row, col]) == Common.COLOR_BLACK:
+        labyrinth[row, col] = [0, 255, 0]
+        if row % 5 == 0:
+            cv2.imshow('Looking for line', labyrinth)
+            cv2.waitKey()
+
+        row -= 1
+
+    row += 1
+
+    print(row, col, 'Found last black tile')
+
+    while tuple(labyrinth[row, col]) != Common.COLOR_WHITE:
+        labyrinth[row, col] = [0, 255, 0]
+        if col % 5 == 0:
+            cv2.imshow('Looking for line', labyrinth)
+            cv2.waitKey()
+
+        col += 1
+
+    print(row, col, 'Found entrance')
+        
+
+    return Point(col, row)
+
+def PlayGame(make_new_labyrinth: bool):
+    if make_new_labyrinth:
+        if not OpenBrowserAndStoreCustomLabyrinth(Common.NUM_ROWS, Common.NUM_COLUMNS, Common.LABYRINTH_PATH):
+            print('Failed to store custom labyrinth')
+
+    labyrinth = GetProcessedLabyrinthImage()
+    start_point = GetStartPoint(labyrinth)
 
     
 
 if __name__ == "__main__":
     Common.initialize_keyboard_listener()
-    PlayGame()
+    PlayGame(False)
